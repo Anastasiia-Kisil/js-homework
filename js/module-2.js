79e11ab912bf3dcccde35dd218678e3aee023942
Anastasiia kisil <hunterr25n@gmail.com>
2021-05-19 21:17:10 +0400

    Update module-2.js

--- js/module-2.js
+++ js/module-2.js
@@ -103,3 +103,66 @@ function calculateEngravingPrice(message, pricePerWord) {
 }
 
 console.log(calculateEngravingPrice('JavaScript is in my blood', 10));
+
+// TASK 12 Дополни код функции makeStringFromArray(array, delimeter) так, чтобы она возвращала в переменной string результат соединения элементов массива array c разделителем delimeter - строку.
+function makeStringFromArray(array, delimeter) {
+    let string;
+    string = array.join(delimeter);
+    return string;
+}
+console.log(
+    makeStringFromArray(['Mango', 'hurries', 'to', 'the', 'train'], ' '),
+);
+
+// TASK 13 Напиши функцию slugify(title) которая принимает заголовок статьи, параметр title, и возвращает slug, созданный из этой строки.
+// Значением параметра title будут строки, слова которых разделены только пробелами
+// Все символы slug должны быть в нижнем регистре
+// Все слова slug должна быть разделены тире
+function slugify(title) {
+    const slug = title.toLowerCase().split(' ').join('-');
+    return slug;
+}
+
+console.log(slugify('Arrays for begginers'));
+
+// TASK 14 Дополни код так, чтобы переменные содержали частичные копии исходного массива fruits.
+// firstTwoEls - массив из первых двух элементов
+// nonExtremeEls - массив из всех элементов кроме первого и последнего
+// lastThreeEls - массив из трёх последних элементов
+const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(1, 4);
+const lastThreeEls = fruits.slice(2, 5);
+
+console.log(lastThreeEls);
+
+// TASK 15 Дополни код так, чтобы в переменной allClients получился массив всех элементов массивов oldClients и newClients.
+const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+const newClients = ['Peach', 'Houston'];
+const allClients = oldClients.concat(newClients);
+
+// TASK 16 Напиши функцию makeArray(firstArray, secondArray, maxLength) для создания нового массива со всеми элементами двух исходных firstArray и secondArray. Параметр maxLength содержит максимально допустимую длину нового массива.
+// Если количество элементов нового массива больше maxLength, функция должна вернуть копию массива длиной maxLength элементов. В противном случае функция должна вернуть новый массив целиком.
+function makeArray(firstArray, secondArray, maxLength) {
+    // Change code below this line
+    let allArray = firstArray.concat(secondArray);
+    if (allArray.length > maxLength) {
+        allArray = allArray.slice(0, maxLength);
+    }
+
+    return allArray;
+    // Change code above this line
+}
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+
+// TASK 17 Дополни цикл for так, чтобы он логировал все целые числа в диапазоне от start до end включительно.
+const start = 3;
+const end = 7;
+
+for (let i = start; i <= end; i += 1) {
+    // Change this line
+    console.log(i);
+}
+
+// TASK 18 Напиши функцию calculateTotal(number), которая принимает целое число (параметр number) и возвращает сумму всех целых чисел от единицы и до этого числа. Например, если number равно 3, то сумма это 1 + 2 + 3, то есть 6
