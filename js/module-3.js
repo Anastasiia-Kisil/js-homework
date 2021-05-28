aa9357321f0763595c818467dba21995308c78c8
Anastasiia kisil <hunterr25n@gmail.com>
2021-05-28 17:32:50 +0400

    end module 3

--- js/module-3.js
+++ js/module-3.js
@@ -556,3 +556,44 @@ const atTheOldToad = {
         this.potions.splice(potionIndex, 1, newName);
     },
 };
+
+// TASK 41
+const atTheOldToad = {
+    potions: [
+        { name: 'Speed potion', price: 460 },
+        { name: 'Dragon breath', price: 780 },
+        { name: 'Stone skin', price: 520 },
+    ],
+    // Change code below this line
+    getPotions() {
+        return this.potions;
+    },
+    addPotion(potionName) {
+        if (this.potions.includes(potionName)) {
+            return `Potion ${potionName} is already equipped!`;
+        }
+
+        this.potions.push(potionName);
+    },
+    removePotion(potionName) {
+        for (const potion of this.potions) {
+            if (potion.name === potionName) {
+                const indexName = this.potions.indexOf(potion);
+                this.potions.splice(indexName, 1);
+                return this.potions;
+            }
+        }
+        return `Potion ${potionName} is not in inventory!`;
+    },
+    updatePotionName(oldName, newName) {
+        const { potions } = this;
+        for (let i = 0; i < potions.length; i += 1) {
+            if (potions[i].name === oldName) {
+                potions[i].name = newName;
+                return potions;
+            }
+        }
+        return `Potion ${oldName} is not in inventory!`;
+    },
+    // Change code above this line
+};
